928a3fb26ebd384e216b1bd1fe944f96259dda54
Marek Dz <marek2901@gmail.com>
2016-06-05 15:32:35 +0200

    Bfs

---
+++ src/com/company/BreadthFirstSearch.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class BreadthFirstSearch {
+
+    void bfs(Vertex root){
+        Queue<Vertex> queue = new LinkedList<>();
+
+        root.setVisited(true);
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            Vertex mVertex = queue.remove();
+            System.out.println(mVertex);
+
+            mVertex.getNeighbours().stream().filter(Vertex::isNotVisited).forEach(vertex -> {
+                vertex.setVisited(true);
+                queue.add(vertex);
+            });
+        }
+    }
+}
---
+++ src/com/company/Vertex.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Vertex {
+
+    private int data;
+
+    private boolean isVisited = false;
+
+    private List<Vertex> neighbours = new ArrayList<>();
+
+    Vertex(int data) {
+        this.data = data;
+    }
+
+    void addNeighbour(Vertex vertex){
+        this.neighbours.add(vertex);
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    boolean isNotVisited() {
+        return !isVisited;
+    }
+
+    void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+
+    List<Vertex> getNeighbours() {
+        return neighbours;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("My value is: %s", this.data);
+    }
+}
